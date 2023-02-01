8ba1f7d14c6ef5db3ecbb812933a2c0ad46cff22
PizzaNCoding88 <110925628+PizzaNCoding88@users.noreply.github.com>
2023-02-01 08:54:31 +0000

    initial-setup

---
+++ script.js
@@ -0,0 +1,22 @@
+const hamburger = document.querySelector(".fa-bars");
+const closeMenu = document.querySelector(".fa-xmark");
+const overlay = document.querySelector(".overlay");
+const navMenu = document.querySelector(".header-navigation-menu")
+
+hamburger.addEventListener("click", openSideBar);
+closeMenu.addEventListener("click", closeSideBar);
+overlay.addEventListener("click", closeSideBar);
+
+function openSideBar(){
+    if(!navMenu.classList.contains("open")){
+        navMenu.classList.add("open");
+        overlay.classList.add("active");
+    }
+}
+
+function closeSideBar(){
+    if(navMenu.classList.contains("open")){
+        navMenu.classList.remove("open");
+        overlay.classList.remove("active");
+    }
+}
