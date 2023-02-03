8e78613f20f32b8c02d483738cf4d149347cdcab
PizzaNCoding88 <110925628+PizzaNCoding88@users.noreply.github.com>
2023-02-03 18:12:12 +0000

    selectors-added--skills-section

--- script.js
+++ script.js
@@ -22,3 +22,34 @@ function closeSideBar(){
         overlay.classList.remove("active");
     }
 }
+
+//SKILLS SELECTOR
+
+const skillSelectorIconLeft = document.querySelector("#skill-selector-icon-left");
+const skillSelectorIconRight = document.querySelector("#skill-selector-icon-right");
+
+const skillsSelectorBoxLeft = document.querySelector("#skills__selector__box-left");
+const skillsSelectorBoxRight = document.querySelector("#skills__selector__box-right");
+
+
+function hooverOnLeft(){
+    skillsSelectorBoxLeft.style.background = "black";
+    skillSelectorIconLeft.src = `https://img.icons8.com/ios/50/null/FFFFFF/roller-brush--v1.png`
+}
+
+function hooverOffLeft(){
+    skillsSelectorBoxLeft.style.background = "transparent";
+    skillSelectorIconLeft.src = `https://img.icons8.com/ios/50/null/roller-brush--v1.png`
+}
+
+function hooverOnRight(){
+    skillsSelectorBoxRight.style.background = "black";
+    skillSelectorIconRight.src = `https://img.icons8.com/ios/50/null/FFFFFF/source-code.png`
+}
+
+function hooverOffRight(){
+    skillsSelectorBoxRight.style.background = "transparent";
+    skillSelectorIconRight.src = `https://img.icons8.com/ios/50/null/source-code.png`
+}
+
+
